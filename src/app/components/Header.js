062016b830ba43bb3c9e15dda8d2f198e02087aa
webangon <angon1436@gmail.com>
2024-05-08 16:11:51 +0600

    All minify

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -45,7 +45,11 @@ export default function Header() {
           <Image data-toggle-sidebar="sidebar1" className="mobile-tap" src="/img/mobile-tap.png" alt="Better Stack" width={50} height={50}/>
         </div>    
         <Sidebar/>    
-      </header>     
+      </header>
+      <Script src="/js/splide.min.js"></Script>
+      <Script src="/js/glightbox.js"></Script>
+      <link href="/css/glightbox.css" rel="stylesheet"></link>
+      <link href="/css/splide.min.css" rel="stylesheet"></link>      
     </>
   )
 }
