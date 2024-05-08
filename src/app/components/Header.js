c265e961472ad56f0a6a6f6ee5275d13deb28948
webangon <angon1436@gmail.com>
2024-05-08 15:57:12 +0600

    Removed splide

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -45,9 +45,7 @@ export default function Header() {
           <Image data-toggle-sidebar="sidebar1" className="mobile-tap" src="/img/mobile-tap.png" alt="Better Stack" width={50} height={50}/>
         </div>    
         <Sidebar/>    
-      </header>
-      <Script src="/js/splide.min.js"></Script>
-      <link href="/css/splide.min.css" rel="stylesheet"></link>      
+      </header>     
     </>
   )
 }
